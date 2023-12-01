ccac68197953868add6b522192561c76d2bd3d69
David Krauth <dakrauth@gmail.com>
2023-12-01 17:10:28 +0900

    Updates for latest dependencies; black

--- castaway.py
+++ castaway.py
@@ -5,34 +5,39 @@ required = object()
 
 
 def cast_bool(val):
-    return val.lower() in {'1', 'yes', 'true', 'y', 'on'} if isinstance(val, str) else bool(val)
+    return (
+        val.lower() in {"1", "yes", "true", "y", "on"}
+        if isinstance(val, str)
+        else bool(val)
+    )
 
 
 def cast_list(val):
-    return [i.strip() for i in val.split(',')]
+    return [i.strip() for i in val.split(",")]
 
 
 def cast_django_db(val):
     import dj_database_url
+
     return dj_database_url.parse(val)
 
 
 def cast_django_email(val):
     import dj_email_url
+
     return dj_email_url.parse(val)
 
 
 class Config:
-
-    def __init__(self, filename='.env', **castings):
+    def __init__(self, filename=".env", **castings):
         self.filename = filename
         self.found_path = None
 
         self.castings = {
             bool: cast_bool,
             list: cast_list,
-            'django_db': cast_django_db,
-            'django_email': cast_django_email,
+            "django_db": cast_django_db,
+            "django_email": cast_django_email,
         }
         self.castings.update(**castings)
 
@@ -51,11 +56,11 @@ class Config:
             value = self.values.get(key, default)
 
         if value is required:
-            raise EnvironmentError(f'{key} is required')
+            raise EnvironmentError(f"{key} is required")
 
         return None if value is None else self.castings.get(cast, cast)(value)
 
 
 def __getattr__(name):
-    if name == 'config':
+    if name == "config":
         return Config()
